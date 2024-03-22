83b5674b548f1978368689ac330bcc360eee3422
weiwei146 <100007345+weiwei146@users.noreply.github.com>
2024-03-22 22:10:40 +0700

    commit

---
+++ src/main/java/checkScoreStudent.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class checkScoreStudent {
+    public String check(String wish, double total, double scoreMath) {
+        String res = "Trượt";
+        switch (wish) {
+            case "Toán":
+                if(total >= 42 && scoreMath >= 8.5) {
+                    res = "Đỗ toán";
+                }
+                break;
+            case "Tin":
+                if(total >= 41 && scoreMath >= 8) {
+                    res = "Đỗ tin";
+                }
+                break;
+            default:
+                if(total >= 42) {
+                    if(scoreMath >= 8.5) {
+                        res = "Đỗ toán";
+                    }
+                    else if(scoreMath >= 8) {
+                        res = "Đỗ tin";
+                    }
+                } else if (total >= 41) {
+                    if(scoreMath >= 8) {
+                        res = "Đỗ tin";
+                    }
+                }
+        }
+        return res;
+    }
+}
---
+++ src/main/java/checkScoreStudentTest.java
@@ -0,0 +1,62 @@
+package org.example;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class checkScoreStudentTest {
+
+    @Test
+    public void test1() {
+        checkScoreStudent checker = new checkScoreStudent();
+        String result = checker.check("Toán", 42, 8.5);
+        assertEquals("Đỗ toán", result);
+    }
+
+    @Test
+    public void test2() {
+        checkScoreStudent checker = new checkScoreStudent();
+        String result = checker.check("Toán", 41, 8);
+        assertEquals("Trượt", result);
+    }
+
+    @Test
+    public void test3() {
+        checkScoreStudent checker = new checkScoreStudent();
+        String result = checker.check("Tin", 41, 8);
+        assertEquals("Đỗ tin", result);
+    }
+
+    @Test
+    public void test4() {
+        checkScoreStudent checker = new checkScoreStudent();
+        String result = checker.check("Tin", 40, 8);
+        assertEquals("Trượt", result);
+    }
+
+    @Test
+    public void test5() {
+        checkScoreStudent checker = new checkScoreStudent();
+        String result = checker.check("", 42, 8.5);
+        assertEquals("Đỗ toán", result);
+    }
+
+    @Test
+    public void test6() {
+        checkScoreStudent checker = new checkScoreStudent();
+        String result = checker.check("", 41, 8.5);
+        assertEquals("Đỗ tin", result);
+    }
+
+    @Test
+    public void test7() {
+        checkScoreStudent checker = new checkScoreStudent();
+        String result = checker.check("", 42, 7);
+        assertEquals("Trượt", result);
+    }
+    @Test
+    public void test8() {
+        checkScoreStudent checker = new checkScoreStudent();
+        String result = checker.check("", 41.5, 8.5);
+        assertEquals("Đỗ tin", result);
+    }
+}
